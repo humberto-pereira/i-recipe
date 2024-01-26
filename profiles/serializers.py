a16f68d684db7aa459a9bb9179501429dd5edc5f
Humberto Pereira <humberto_pereira@live.com>
2024-01-26 18:04:18 +0100

    added login/logout views, is_user permission and the is_user field to profile serializer

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -3,9 +3,15 @@ from .models import Profile
 
 class ProfileSerializer(serializers.ModelSerializer):
     user = serializers.ReadOnlyField(source='user.username')
+    is_user = serializers.SerializerMethodField()
+
+    def get_is_user(self, obj):
+        request = self.context.get('request')
+        return request.user == obj.user
+    
     class Meta:
         model = Profile
         fields = [
-            'id', 'user', 'created_at', 'updated_at', 'name', 'content', 'image'
+            'id', 'user', 'created_at', 'updated_at', 'name', 'content', 'image', 'is_user'
         ] # 'id' is created automatically by Django
 
