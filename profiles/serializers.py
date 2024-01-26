529976e73ed40d002da544800116d90a97860422
Humberto Pereira <humberto_pereira@live.com>
2024-01-26 14:11:14 +0100

    Add profiles serializer and views.py

---
+++ profiles/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from .models import Profile
+
+class ProfileSerializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source='user.username')
+    class Meta:
+        model = Profile
+        fields = [
+            'id', 'user', 'created_at', 'updated_at', 'name', 'content', 'image'
+        ]
