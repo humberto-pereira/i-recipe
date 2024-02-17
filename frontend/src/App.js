97491c3a4160449da05b3a79668271842070b237
Humberto Pereira <humberto_pereira@live.com>
2024-02-17 21:31:29 +0100

    add code to display logged in user in the navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,36 @@
 // src/App.js
-import React from 'react';
+import React, { createContext } from 'react';
 import NavBar from './components/NavBar';
 import styles from './App.module.css';
 import Container from 'react-bootstrap/Container';
 import { Route, Switch } from 'react-router-dom';
 import SignUpForm from './pages/auth/SignUpForm';
 import SignInForm from './pages/auth/SignInForm';
-import './api/axiosDefaults';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+export const CurrentUserContext = createContext();
+export const SetCurrentUserContext = createContext();
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null)
+
+  const handleMount = async () => {
+    try {
+      const { data } = await axios.get('/dj-rest-auth/user/')
+      setCurrentUser(data)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  useEffect(() => {
+    handleMount()
+  }, [])
+
   return (
+    <CurrentUserContext.Provider value={currentUser}>
+      <SetCurrentUserContext.Provider value={setCurrentUser}>
     <div className={styles.App}>
       <NavBar />
       <Container className={styles.Main}>
@@ -21,6 +42,8 @@ function App() {
         </Switch>
       </Container>
     </div>
+    </SetCurrentUserContext.Provider>
+    </CurrentUserContext.Provider>
   );
 }
 
--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,23 +1,42 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Navbar, Nav, NavDropdown, Form, FormControl, Button, Container } from 'react-bootstrap';
 import irecipe_logo from '../assets/irecipe_logo.png';
 import styles from '../styles/NavBar.module.css';
 import { NavLink } from 'react-router-dom';
+import { CurrentUserContext } from '../App';
 
 const NavBar = () => {
+    const currentUser = useContext(CurrentUserContext);
+    const loggedInIcons = <>{currentUser?.username}</>
+    const loggedOutIcons = ( 
+        <> 
+            <NavLink className={styles.NavLink} 
+            activeClassName={styles.Active} 
+            to="/signin" 
+            >
+                <i className='fas fa-sign-in-alt'></i> Sign in
+            </NavLink>
+            <NavLink className={styles.NavLink} 
+            activeClassName={styles.Active} 
+            to="signup">
+                <i className='fas fa-user-plus'></i> Sign up
+            </NavLink>
+        </>
+    );
+
     return (
         <Navbar bg="light" expand="md" fixed='top'>
             <Container>
                 <NavLink to="/">
                     <Navbar.Brand><img src={irecipe_logo} alt='logo' height={45} /></Navbar.Brand>
                 </NavLink>
+                
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ml-auto">
-                    <NavLink exact className={styles.NavLink} activeClassName={styles.Active} to="/"><i className='fas fa-home'></i> Home</NavLink>
-                    <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signin" ><i className='fas fa-sign-in-alt'></i> Sign in</NavLink>
-                    <NavLink className={styles.NavLink} activeClassName={styles.Active} to="signup"><i className='fas fa-user-plus'></i> Sign up</NavLink>
-                    
+                    <NavLink exact className={styles.NavLink} activeClassName={styles.Active} to="/"><i className='fas fa-home'></i>Home
+                    </NavLink>
+                    {currentUser ? loggedInIcons : loggedOutIcons}
                 </Nav>
                 <Form inline>
                     <FormControl type="text" placeholder="Search" className="mr-sm-2" />
